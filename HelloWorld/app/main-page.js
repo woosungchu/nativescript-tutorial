c778d2436929e5c653d1f71442926d629f93549e
woosungchu <woosungchu@gmail.com>
2018-01-28 01:27:19 +0900

    fin chapt3 && add js files

---
+++ HelloWorld/app/main-page.js
@@ -0,0 +1,17 @@
+exports.pageLoaded = function() {
+
+  //1.
+  // console.log("Hello World");
+
+  //2.
+  // console.log({
+  //   type: "Apple",
+  //   color: "Red"
+  // });
+
+  //3. use dir
+  console.dir({
+    type: "Apple",
+    color: "Red"
+  });
+};
